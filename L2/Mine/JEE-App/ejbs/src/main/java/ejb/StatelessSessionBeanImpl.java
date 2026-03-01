480e6b9f9bdc27728bc39853ee783616de2a91ba
AlezChickMLG <alexandru.sova@student.tuiasi.ro>
2026-03-01 19:19:15 +0200

    Init functional

--- L2/Mine/JEE-App/ejbs/src/main/java/ejb/StatelessSessionBeanImpl.java
+++ L2/Mine/JEE-App/ejbs/src/main/java/ejb/StatelessSessionBeanImpl.java
@@ -1,28 +1,24 @@
 package ejb;
-
 import interfaces.StatelessSessionBeanRemote;
-
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+public class StatelessSessionBeanImpl implements
+        StatelessSessionBeanRemote, Serializable {
 
-public class StatelessSessionBeanImpl implements StatelessSessionBeanRemote, Serializable{
     public StatelessSessionBeanImpl() {
-        System.out.println("[Glassfish] S-a instantiat un stateless session bean: " +
-                StatelessSessionBeanImpl.class.getName());
+        System.out.println("[Glassfish] S-a instanţiat un stateles session bean: " +
+        StatelessSessionBeanImpl.class.getName());
     }
-
-    @Override
     public String getCurrentTime() {
         System.out.println("[Glassfish] S-a apelat metoda getCurrentTime()");
         Date date = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
         return formatter.format(date);
     }
-
-    @Override
     public Integer addNumbers(Integer a, Integer b) {
-        System.out.println("[Glassfish] S-a apelat metoda addNumbers(" + a + ", " + b + ")");
+        System.out.println("[Glassfish] S-a apelat metoda addNumbers("
+                + a + ", " + b + ")");
         return a + b;
     }
 }
--- L2/Mine/JEE-App/ejbs/src/main/java/interfaces/StatelessSessionBeanRemote.java
+++ L2/Mine/JEE-App/ejbs/src/main/java/interfaces/StatelessSessionBeanRemote.java
@@ -1,5 +1,4 @@
 package interfaces;
-
 public interface StatelessSessionBeanRemote {
     String getCurrentTime();
     Integer addNumbers(Integer a, Integer b);
